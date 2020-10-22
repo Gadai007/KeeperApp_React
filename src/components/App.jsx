de99589a173b310cbdda7af3e59523642abb2961
Your Name <you@example.com>
2020-10-22 20:21:36 +0530

    notes are added and rendered properly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Note from './Note'
-import notes from '../notes'
+import CreateArea from './CreateArea'
+
 
 const App = () => {
+
+    const [ save, setSave ] = useState([])
+
+    const addNote = (note) => {
+        setSave(prevNote => {
+            return [
+                ...prevNote, note
+            ]
+        })
+    }
+
     return (
         <div>
             <Header />
-            {notes.map(note => {
-                return <Note 
-                        key={note.key}
-                        title={note.title}
-                        content={note.content}/>
+            <CreateArea onAdd={addNote}/>
+            {save.map((note, index) => {
+                return <Note key={index} title={note.title} content={note.content} />
             })}
             <Footer/>
         </div>
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+
+  const [ notes, setNotes ] = useState({
+    title:'',
+    content: ''
+  })
+
+  const noteChangeHandler = (event) => {
+      const { name, value } = event.target
+      setNotes(prevNotes => {
+        return {
+          ...prevNotes,
+          [name]: value
+        }
+      })
+  }
+
+  const noteSaveHandler = (event) => {
+    event.preventDefault()
+    props.onAdd(notes)
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          onChange={noteChangeHandler} 
+          name="title" 
+          placeholder="Title"
+          value={notes.title} />
+        <textarea
+          onChange={noteChangeHandler}  
+          name="content" 
+          placeholder="Take a note..." 
+          rows="3"
+          value={notes.content} />
+        <button 
+          onClick={noteSaveHandler}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
